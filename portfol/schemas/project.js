2f3a85315e04a0f49b249934e43d276b2ae14f02
adarsh-3099 <65357033+adarsh-3099@users.noreply.github.com>
2021-08-23 17:06:21 +0530

    Add files via upload

---
+++ portfol/schemas/project.js
@@ -0,0 +1,34 @@
+export default {
+    name: "project",
+    title: "Project",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            type: "string"
+        },
+        {
+            name: "date",
+            type: "datetime"
+        },
+        {
+            name: "description",
+            type: "text"
+        },
+        {
+            name: "projectType",
+            title: "Project Type",
+            type: "string",
+            options: {
+                list: [
+                    { value: "personal", title: "Personal" },
+                    { value: "group", title: "College" },
+                ]
+            }
+        },
+        {
+            name: "link",
+            type: "url"
+        }
+    ]
+}
