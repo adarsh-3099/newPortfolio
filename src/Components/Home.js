7a26cc94ed669082d4631e0c8ef21f00ff5ead6e
adarsh-3099 <65357033+adarsh-3099@users.noreply.github.com>
2021-08-23 17:04:29 +0530

    Add files via upload

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <main className="bg-blue-100">
+            <section className="relative flex justify-center min-h-screen pt-2 lg:pt-64 px-6">
+                <h1 className="text-4xl text-blue-400 font-bold leading-none lg:leading-snug home-name">Hello, I am Adarsh.</h1>
+            </section>
+        </main>
+    )
+}
+
+export default Home
