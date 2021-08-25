5f3084350b8a1e32911f33acb7e69456a603ee5f
adarsh-3099 <65357033+adarsh-3099@users.noreply.github.com>
2021-08-25 09:17:00 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
     return (
-        <main className="main ">
+        <main className="main bg-blue-100">
             <section className="name-section">
                 <h1 className="name">Hello, I am Adarsh.</h1>
             </section>
